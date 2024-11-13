e9769e1b1fcf8353c4583cb3d0ea4fd8e30db417
Taher Chabra <redsign12345@gmail.com>
2024-11-13 16:59:59 +0530

    server url updated

--- src/lib/url.js
+++ src/lib/url.js
@@ -1,7 +1,9 @@
 module.exports = {
-  client:
-    process.env.NODE_ENV === 'production'
+  client: process.env.NODE_ENV === 'production'
       ? 'client-url'
       : 'http://localhost:3000',
-  server: 'http://localhost:5100',
+
+  server: process.env.NODE_ENV === 'production'
+      ? 'https://talktime-server-16uu.onrender.com'
+      : 'http://localhost:5100',
 };
