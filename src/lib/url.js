a14e5d679529abe548e4a1e9eecbe7460f9eb24a
Taher Chabra <redsign12345@gmail.com>
2024-11-24 13:26:23 +0530

    fixes

--- src/lib/url.js
+++ src/lib/url.js
@@ -3,7 +3,9 @@ module.exports = {
       ? 'https://talktime-app.onrender.com'
       : 'http://localhost:3000',
 
-  server: process.env.NODE_ENV === 'production'
-      ? 'https://talktime-server-16uu.onrender.com'
-      : 'http://localhost:5100',
+    server: 'https://ws.msft.lohani.dev'
+
+//   server: process.env.NODE_ENV === 'production'
+//       ? 'https://ws.msft.lohani.dev'
+//       : 'http://localhost:5100',
 };
